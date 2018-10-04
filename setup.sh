f61b1ac3ed11fa00de3967a56cd85bd9af5db5cc
Yoav Ram <yoav@yoavram.com>
2018-10-04 21:13:52 +0300

    install pacakges

---
+++ setup.sh
@@ -0,0 +1,2 @@
+conda install numpy matplotlib scipy sympy networkx numba ipywidgets seaborn pandas cartopy autograd tensorflow scikit-image scikit-learn statsmodels -y -q -c conda-forge
+pip install pyabc
