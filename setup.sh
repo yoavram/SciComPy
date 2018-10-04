f0e575250a27420d767912c454bd7aac3c21ecff
Yoav Ram <yoav@yoavram.com>
2018-10-04 22:39:14 +0300

    not working

--- setup.sh
+++
@@ -1,4 +0,0 @@
-conda update conda pip -y -q
-conda update numpy matplotlib scipy pandas seaborn statsmodels sympy tensorflow ipywidgets keras networkx numba scikit-image scikit-learn  -y -q
-conda install cartopy autograd -y -q
-pip install pyabc
