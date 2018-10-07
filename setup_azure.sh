550058b42b81d0dc598ca0a42e9e7f5d0dd16dd0
Yoav Ram <yoav@yoavram.com>
2018-10-07 20:22:42 +0300

    h5py for keras

--- setup_azure.sh
+++ setup_azure.sh
@@ -2,3 +2,4 @@
 ~/anaconda3_501/bin/conda update networkx -y -q
 ~/anaconda3_501/bin/python -m pip install pyABC
 ~/anaconda3_501/bin/conda update tensorflow -y -q
+~/anaconda3_501/bin/conda install h5py
