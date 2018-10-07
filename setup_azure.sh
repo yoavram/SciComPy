6a9dc1355b8b4853eabb549267d973f03f8462c8
Yoav Ram <yoav@yoavram.com>
2018-10-07 19:32:48 +0300

    networkx version

--- setup_azure.sh
+++ setup_azure.sh
@@ -1,3 +1,4 @@
 ~/anaconda3_501/bin/conda update conda pip -y -q
-~/anaconda3_501/bin/conda update tensorflow -y -q
+~/anaconda3_501/bin/conda update networkx -y -q
 ~/anaconda3_501/bin/python -m pip install pyABC
+~/anaconda3_501/bin/conda update tensorflow -y -q
