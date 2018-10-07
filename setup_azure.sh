61b3afcc9c0b3c30fd3d2fe369a4d087361bdf9e
Yoav Ram <yoav@yoavram.com>
2018-10-07 20:47:14 +0300

    update pandas: its only 0.20

--- setup_azure.sh
+++ setup_azure.sh
@@ -1,4 +1,5 @@
 ~/anaconda3_501/bin/conda update conda pip -y -q
+~/anaconda3_501/bin/conda update pandas -y -q
 ~/anaconda3_501/bin/conda update networkx -y -q
 ~/anaconda3_501/bin/python -m pip install pyABC
 ~/anaconda3_501/bin/conda update tensorflow -y -q
