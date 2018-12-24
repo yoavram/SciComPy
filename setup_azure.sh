d71d35d4037d1e676caf438b2da12ce60fad2b60
Yoav Ram <yoav@yoavram.com>
2018-12-24 14:30:12 +0200

    copied from scicompy

--- setup_azure.sh
+++
@@ -1,6 +0,0 @@
-~/anaconda3_501/bin/conda update conda pip -y -q
-~/anaconda3_501/bin/conda update pandas -y -q
-~/anaconda3_501/bin/conda update networkx -y -q
-~/anaconda3_501/bin/python -m pip install pyABC
-~/anaconda3_501/bin/conda update tensorflow -y -q
-~/anaconda3_501/bin/conda install h5py
