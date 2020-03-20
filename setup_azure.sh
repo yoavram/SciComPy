2f8d346b1738870439d12ad880419d6d5b35a47a
Yoav Ram <yoav@yoavram.com>
2020-03-20 16:09:47 +0200

    py3.7

--- setup_azure.sh
+++ setup_azure.sh
@@ -1,6 +1,7 @@
 ~/anaconda3_501/bin/conda update conda pip -y -q
+~/anaconda3_501/bin/conda install python=3.7 -y -q
 ~/anaconda3_501/bin/conda update pandas -y -q
 ~/anaconda3_501/bin/conda update networkx -y -q
-~/anaconda3_501/bin/python -m pip install pyABC
+~/anaconda3_501/bin/python -m pip install pyABC -q
 ~/anaconda3_501/bin/conda update tensorflow -y -q
-~/anaconda3_501/bin/conda install h5py
+~/anaconda3_501/bin/conda install h5py -y -q
