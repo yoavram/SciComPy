03b0a256b8804b35fee5695358171c2cb5c1d332
Yoav Ram <yoav@yoavram.com>
2020-04-19 18:30:37 +0300

    corner

--- setup_azure.sh
+++ setup_azure.sh
@@ -5,3 +5,4 @@
 ~/anaconda3_501/bin/python -m pip install pyABC -q
 ~/anaconda3_501/bin/conda update tensorflow -y -q
 ~/anaconda3_501/bin/conda install h5py -y -q
+~/anaconda3_501/bin/conda install corner -y -q
